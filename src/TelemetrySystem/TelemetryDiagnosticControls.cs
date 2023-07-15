1ea18a3691f281ed48c8133e4e139eeab1107524
Saini45 <79507373+Saini45@users.noreply.github.com>
2023-07-15 12:46:06 +0530

    Add files via upload

---
+++ src/TelemetrySystem/TelemetryDiagnosticControls.cs
@@ -0,0 +1,49 @@
+
+using System;
+
+namespace TDDMicroExercises.TelemetrySystem
+{
+    public class TelemetryDiagnosticControls
+    {
+        private const string DiagnosticChannelConnectionString = "*111#";
+
+        //private readonly TelemetryClient _telemetryClient;
+        private readonly ITelemetryClient _ItelemetryClient;
+        private string _diagnosticInfo = string.Empty;
+
+        public TelemetryDiagnosticControls()
+        {
+            _ItelemetryClient = new TelemetryClient();
+        }
+
+        public string DiagnosticInfo
+        {
+            get { return _diagnosticInfo; }
+            set { _diagnosticInfo = value; }
+        }
+
+        public bool OnlineStatus => throw new NotImplementedException();
+
+        public void CheckTransmission()
+        {
+            _diagnosticInfo = string.Empty;
+
+            _ItelemetryClient.Disconnect();
+
+            int retryLeft = 3;
+            while (_ItelemetryClient.OnlineStatus == false && retryLeft > 0)
+            {
+                _ItelemetryClient.Connect(DiagnosticChannelConnectionString);
+                retryLeft -= 1;
+            }
+             
+            if(_ItelemetryClient.OnlineStatus == false)
+            {
+                throw new Exception("Unable to connect.");
+            }
+
+            _ItelemetryClient.Send(TelemetryClient.DiagnosticMessage);
+            _diagnosticInfo = _ItelemetryClient.Receive();
+        }
+    }
+}
---
+++ src/TirePressureMonitoringSystem/Alarm.cs
@@ -0,0 +1,32 @@
+namespace TDDMicroExercises.TirePressureMonitoringSystem
+{
+    public class Alarm
+    {
+        private const double LowPressureThreshold = 17;
+        private const double HighPressureThreshold = 21;
+
+        //readonly Sensor _sensor = new Sensor();
+        private ISensor _sensor;
+        bool _alarmOn = false;
+        public Alarm()
+        {
+            this._sensor = new Sensor();
+            //_alarmOn = false;
+        }
+        public void Check()
+        {
+            double psiPressureValue = _sensor.PopNextPressurePsiValue();
+
+            if (psiPressureValue < LowPressureThreshold || HighPressureThreshold < psiPressureValue)
+            {
+                _alarmOn = true;
+            }
+        }
+
+        public bool AlarmOn
+        {
+            get { return _alarmOn; }
+        }
+
+    }
+}
